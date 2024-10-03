a93986ac890874d3e9530ee8e54bec9c3cbe7d66
Beaast-exe <r.fariaa18@gmail.com>
2024-10-03 13:38:56 +0100

    project refactor

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,8 +1,19 @@
 module.exports = {
-    database: {
-        host: 'localhost',
-        user: 'Dashboard',
-        password: 'dashboard.pass.db',
-        database: 'Dashboard'
-    }
+	server: {
+		port: 3000,
+		corsPort: 3001
+	},
+	database: {
+		host: '127.0.0.1',
+		user: 'Dashboard',
+		password: 'dashboard.pass.db',
+		database: 'Dashboard',
+		dialect: 'mysql',
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		}
+	}
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,68 +1,19 @@
 const express = require('express');
 const app = express();
 
-const mysql = require('mysql');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
-const { host, user, password, database } = require('./config')
+const db = require('./models');
+db.sequelize.sync().then(() => {
+	console.log('Drop and re-sync db.');
+});
 
-const db = mysql.createPool({ host, user, password, database });
+const { port, corsPort } = require('./config').server
 
+const corsOptions = { origin: `http://localhost:${corsPort}` };
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/register', (req, res) => {
-	const { email, password } = req.body;
-
-	db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
-		if (err) {
-			res.send(err);
-		}
-
-		if (result.length == 0) {
-			bcrypt.hash(password, saltRounds, (err, hash) => {
-				db.query(
-					'INSERT INTO users (email, password) VALUE (?,?)',
-					[email, hash],
-					(error, response) => {
-						if (err) {
-							res.send(err);
-						}
-
-						res.send({ msg: 'User created successfully!' });
-					}
-				);
-			})
-		}
-	});
-});
-
-app.post('/login', (req, res) => {
-	const { email, password } = req.body;
-
-	db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
-		if (err) {
-			res.send(err);
-		}
-
-		if (result.length > 0) {
-			bcrypt.compare(password, result[0].password, (error, response) => {
-				if (error) {
-					res.send(error);
-				}
-
-				if (response == true) {
-					res.send(response);
-				} else {
-					res.send({ msg: 'Invalid email or password.' });
-				}
-			});
-		} else {
-			res.send({ msg: 'There is no user registered with that email.' });
-		}
-	});
-});
-
-app.listen(3001, () => console.log('Server listening on port 3001'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
