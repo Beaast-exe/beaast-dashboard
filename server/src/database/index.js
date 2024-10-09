3f953cb08076b51a203d2c40ce54705ef03f8e5e
Beaast-exe <r.fariaa18@gmail.com>
2024-10-09 09:08:25 +0100

    finished techs structure

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -3,13 +3,16 @@ const dbConfig = require('../config/database');
 
 const User = require('../models/User');
 const Address = require('../models/Address');
+const Tech = require('../models/Tech');
 
 const sequelize = new Sequelize(dbConfig);
 
 User.init(sequelize);
 Address.init(sequelize);
+Tech.init(sequelize);
 
 Address.associate(sequelize.models);
 User.associate(sequelize.models);
+Tech.associate(sequelize.models);
 
 module.exports = sequelize;
